750011f19835a8e3b8f45769441edbc2ce9900e5
MuktaPatil <72442967+MuktaPatil@users.noreply.github.com>
2021-02-24 09:41:20 +0530

    Add files via upload
    IF shell script using the continue and break

---
+++ if.sh
@@ -0,0 +1,10 @@
+for a in 1 2 3 4 5 6 7 8 9 10
+
+do
+  if [ $a == 5 ] 
+  then break
+else
+   echo "Iteration no $a"
+fi
+done
+
---
+++ loopex.sh
@@ -0,0 +1,16 @@
+3)for a in 1 2 3 4 5 6 7 8 9 10 
+do
+#if a = 5 then continue the loop and don't move to line 8
+
+if [ $a == 5 ]
+
+then
+
+exit
+
+fi
+
+echo "Iteration no $a"
+
+done
+
